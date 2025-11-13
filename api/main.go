b9a7c19019059df3827d9e5c8f41dff374a1a16b
prasdud <prasdudwastaken@gmail.com>
2025-11-13 22:26:53 +0530

    playing around with Gin

---
+++ api/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+
+func pingHandler(c *gin.Context){
+	c.JSON(200, gin.H{"message": "pong"})
+}
+
+
+func main(){
+	
+	r := gin.Default()
+
+	r.GET("/ping", pingHandler)
+
+	r.Run()
+}
