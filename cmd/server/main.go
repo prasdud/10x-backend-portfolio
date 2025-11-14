e834d8908c12d00d7a4e4d4c9946fe20c321caf0
prasdud <prasdudwastaken@gmail.com>
2025-11-14 09:56:18 +0530

    fix: exported funcs were not capitalized

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,34 +3,23 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/prasdud/10x-backend-portfolio/internal/handlers"
-
 )
 
+func main() {
 
-func pingHandler(c *gin.Context){
-	c.JSON(200, gin.H{"message": "pong"})
-}
-
-
-func main(){
-	
 	r := gin.Default()
-	
+
 	v1 := r.Group("/api/v1")
-	v1.GET("/ping", handlers.ping)
-	v1.GET("/resume", handlers.getResume)
-	v1.GET("/skills", handlers.getSkills)
-	v1.GET("/projects", handlers.getProjects)
-	v1.GET("/experience", handlers.getExperience)
-	v1.GET("/contact", handlers.getContact)
-	v1.GET("/blog", handlers.getBlog)
-	v1.GET("/about", handlers.getAbout)
-	v1.GET("/easteregg1", handlers.getEasterEgg1)
-	v1.GET("/easteregg2", handlers.getEasterEgg2)
-
-
-
-	r.GET("/ping", pingHandler)
+	v1.GET("/ping", handlers.Ping)
+	v1.GET("/resume", handlers.GetResume)
+	v1.GET("/skills", handlers.GetSkills)
+	v1.GET("/projects", handlers.GetProjects)
+	v1.GET("/experience", handlers.GetExperience)
+	v1.GET("/contact", handlers.GetContact)
+	v1.GET("/blog", handlers.GetBlog)
+	v1.GET("/about", handlers.GetAbout)
+	v1.GET("/easteregg1", handlers.GetEasterEgg1)
+	v1.GET("/easteregg2", handlers.GetEasterEgg2)
 
 	r.Run(":7741")
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -2,74 +2,64 @@ package handlers
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/prasdud/10x-backend-portfolio/internal/handlers"
 )
 
-func ping(c *gin.Context){
+func Ping(c *gin.Context) {
 	c.JSON(200, gin.H{"message": "pong"})
 }
 
-
-func getResume(c *gin.Context){
+func GetResume(c *gin.Context) {
 	c.File("https://prasdud.com/314159265358.pdf")
 }
 
-func getSkills(c *gin.Context){
+func GetSkills(c *gin.Context) {
 	c.JSON(200, gin.H{
-			"Python": "Master",
-			"Go":"Proficient",
+		"Python": "Master",
+		"Go":     "Proficient",
 	})
 }
 
-
-func getProjects(c *gin.Context){
+func GetProjects(c *gin.Context) {
 	c.JSON(200, gin.H{
-			"DISAC": "Data integrity system for academic credentials",
-			"Raptor":"Red team AI malware simulator",
+		"DISAC":  "Data integrity system for academic credentials",
+		"Raptor": "Red team AI malware simulator",
 	})
 }
 
-
-func getExperience(c *gin.Context){
+func GetExperience(c *gin.Context) {
 	c.JSON(200, gin.H{
-			"Omni-Tech Solutions": "SWE Intern",
-			"Dragbin":"SWE Intern",
+		"Omni-Tech Solutions": "SWE Intern",
+		"Dragbin":             "SWE Intern",
 	})
 }
 
-
-func getContact(c *gin.Context){
+func GetContact(c *gin.Context) {
 	c.JSON(200, gin.H{
-			"Linkedin": "LINKEDINURL",
-			"X / Twitter": "XURL",
-			"Phone Number": "XXXXXXXX",
-			"Email": "X@Y.com",
+		"Linkedin":     "LINKEDINURL",
+		"X / Twitter":  "XURL",
+		"Phone Number": "XXXXXXXX",
+		"Email":        "X@Y.com",
 	})
 }
 
-
-func getBlog(c *gin.Context){
-	c.Redirect(302, "https://blog.prasdud.com"
+func GetBlog(c *gin.Context) {
+	c.Redirect(302, "https://blog.prasdud.com")
 }
 
-
-func getAbout(c *gin.Context){
+func GetAbout(c *gin.Context) {
 	c.JSON(200, gin.H{
-			"Abdul": "who is he"
+		"Abdul": "who is he",
 	})
 }
 
-
-func getEasterEgg1(c *gin.Context){
+func GetEasterEgg1(c *gin.Context) {
 	c.JSON(200, gin.H{
-			"Easter egg ": "#1"
+		"Easter egg ": "#1",
 	})
 }
 
-func getEasterEgg2(c *gin.Context){
+func GetEasterEgg2(c *gin.Context) {
 	c.JSON(200, gin.H{
-			"Easter egg": "#2"
+		"Easter egg": "#2",
 	})
 }
-
-
